ebef127ab06a236d4c5eed40a011ac171cffa12d
hugosrc <hugo.uibai54@gmail.com>
2020-08-05 18:29:11 -0300

    feat: Create Header Page Overview

--- src/pages/Overview/index.tsx
+++ src/pages/Overview/index.tsx
@@ -1,12 +1,40 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Feather as Icon } from '@expo/vector-icons';
+import { View, TouchableOpacity, Text } from 'react-native';
+import { useNavigation, DrawerActions } from '@react-navigation/native';
+
 import FabButton from '../../components/FabButton';
 
 import styles from './styles'
 
 const Overview: React.FC = () => {
+  const navigation = useNavigation()
+
+  function toggleDrawer() {
+    navigation.dispatch(DrawerActions.openDrawer())
+  }
+
   return (
     <View style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={toggleDrawer}>
+          <Icon name="menu" color="#fff" size={22} />
+        </TouchableOpacity>
+
+        <Text style={styles.title}>Budgets</Text>
+
+        <View style={styles.card}>
+          <View style={styles.cardHeader}>
+            <Text style={styles.headerTitle}>Total</Text>
+            <TouchableOpacity>
+              <Icon name="edit-3" color="#444" size={22} />
+            </TouchableOpacity>
+          </View>
+
+          <Text style={styles.cartValue}>$58,410</Text>
+        </View>
+      </View>
+
       <FabButton
         pressIncome={() => {}}
         pressExpense={() => {}}
--- src/pages/Overview/styles.ts
+++ src/pages/Overview/styles.ts
@@ -3,5 +3,48 @@ import { StyleSheet } from "react-native";
 export default StyleSheet.create({
   container: {
     flex: 1
+  },
+
+  header: {
+    alignSelf: "stretch",
+    height: 256,
+    backgroundColor: '#F2994A',
+    paddingTop: 64,
+    paddingHorizontal: 24
+  },
+
+  title: {
+    fontSize: 36,
+    fontWeight: "bold",
+    marginVertical: 16,
+    color: '#fff'
+  },
+
+  card: {
+    height: 172,
+    alignSelf: "stretch",
+    backgroundColor: '#fff',
+    elevation: 4,
+    borderRadius: 14,
+    padding: 24
+  },
+
+  cardHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+
+  headerTitle: {
+    fontSize: 22,
+    fontWeight: "bold",
+    color: '#444'
+  },
+
+  cartValue: {
+    fontSize: 62,
+    fontWeight: "bold",
+    color: '#444',
+    marginTop: 12
   }
 })
